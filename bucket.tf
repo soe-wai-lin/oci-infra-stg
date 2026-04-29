61bbb2795b7e466c4ba6d8f94c6e13e0181b1471
wailin-tw <wailin.s@trinitywizards.com>
2026-04-29 10:08:43 +0800

    changed to bucket public access

--- bucket.tf
+++ bucket.tf
@@ -1,23 +1,23 @@
-data "oci_objectstorage_namespace" "ns" {
-  compartment_id = oci_identity_compartment.data_compartment.id
-}
+# data "oci_objectstorage_namespace" "ns" {
+#   compartment_id = oci_identity_compartment.data_compartment.id
+# }
 
-resource "oci_objectstorage_bucket" "bucket" {
-  compartment_id = oci_identity_compartment.data_compartment.id
-  namespace      = data.oci_objectstorage_namespace.ns.namespace
-  name           = var.bucket_name
+# resource "oci_objectstorage_bucket" "bucket" {
+#   compartment_id = oci_identity_compartment.data_compartment.id
+#   namespace      = data.oci_objectstorage_namespace.ns.namespace
+#   name           = var.bucket_name
 
-  access_type           = var.access_type
-  storage_tier          = var.storage_tier
-  auto_tiering          = var.auto_tiering
-  versioning            = var.versioning
-  object_events_enabled = var.object_events_enabled
-  kms_key_id            = var.kms_key_id
-  metadata              = var.metadata
-  freeform_tags         = var.freeform_tags
-}
+#   access_type           = var.access_type
+#   storage_tier          = var.storage_tier
+#   auto_tiering          = var.auto_tiering
+#   versioning            = var.versioning
+#   object_events_enabled = var.object_events_enabled
+#   kms_key_id            = var.kms_key_id
+#   metadata              = var.metadata
+#   freeform_tags         = var.freeform_tags
+# }
 
-data "oci_objectstorage_bucket" "bucket" {
-  name      = oci_objectstorage_bucket.bucket.name
-  namespace = data.oci_objectstorage_namespace.ns.namespace
-}
+# data "oci_objectstorage_bucket" "bucket" {
+#   name      = oci_objectstorage_bucket.bucket.name
+#   namespace = data.oci_objectstorage_namespace.ns.namespace
+# }
